df115bf12759edac1d7f20fde0b4fa05ea64d873
hthuz <hanggang.20@intl.zju.edu.cn>
2023-06-09 20:07:34 +0800

    Modify: lazy.nvim ui icon display

--- lua/plug.lua
+++ lua/plug.lua
@@ -2,15 +2,16 @@
 -- Auto install lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
+
 
 vim.opt.rtp:prepend(lazypath)
 
@@ -110,6 +111,23 @@ require("lazy").setup({
     },
 
 
-})
+}, { -- Config for lazy
+        ui = {
+            icons = {
+                cmd = "⌘",
+                config = "🛠",
+                event = "📅",
+                ft = "📂",
+                init = "⚙",
+                keys = "🗝",
+                plugin = "🔌",
+                runtime = "💻",
+                source = "📄",
+                start = "🚀",
+                task = "📌",
+                lazy = "💤 ",
+            },
+        },
+    })
 
 require('config/nvim-lspconfig')
