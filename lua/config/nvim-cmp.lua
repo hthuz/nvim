73a4aee70c0edbc72a899889d1ec9f52c4db200d
hthuz <hanggang.20@intl.zju.edu.cn>
2024-05-16 00:21:07 +0800

    update: remap cmp scroll docs

--- lua/config/nvim-cmp.lua
+++ lua/config/nvim-cmp.lua
@@ -82,8 +82,8 @@ cmp.setup({
 
 		end, {'i','s','c'}),
 
-        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+        ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-d>'] = cmp.mapping.scroll_docs(4),
         ['<C-Space>'] = cmp.mapping.complete(),
         ['<C-e>'] = cmp.mapping.abort(),
         ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
