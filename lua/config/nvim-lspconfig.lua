3f9f936639663228b3659be8d4969284574afaaa
hthuz <hanggang.20@intl.zju.edu.cn>
2024-08-05 11:01:36 +0800

    add: plugin codeium

--- lua/config/nvim-lspconfig.lua
+++ lua/config/nvim-lspconfig.lua
@@ -5,6 +5,7 @@ local lspconfig = require('lspconfig')
 -- pacman -S pyright
 lspconfig.pyright.setup {}
 -- lspconfig.pylsp.setup{}
+-- pacman -S lua-language-server
 lspconfig.lua_ls.setup {
     settings = {
         Lua = {
@@ -22,12 +23,11 @@ lspconfig.lua_ls.setup {
     }
 
 }
-
+-- pacman -S bash-language-server
 lspconfig.bashls.setup{}
-
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities.textDocument.completion.completionItem.snippetSupport = true
-
+-- pacman -S clangd
 lspconfig.clangd.setup {}
 lspconfig.html.setup {
     capabilities = capabilities,
@@ -37,28 +37,24 @@ lspconfig.tsserver.setup {}
 lspconfig.cssls.setup {
     capabilities = capabilities,
 }
+-- pacman -S tailwindcss-language-server
 lspconfig.tailwindcss.setup {}
+-- pacman -S marksman
 lspconfig.marksman.setup {}
 
 lspconfig.jdtls.setup {}
 
-
 lspconfig.r_language_server.setup{}
 
 lspconfig.svls.setup {}
-
 -- lspconfig.solc.setup {}
 lspconfig.solidity_ls_nomicfoundation.setup {}
-
 -- npm i -g vscode-langservers-extracted
 lspconfig.jsonls.setup {}
-
 -- pacman -S gopls
 lspconfig.gopls.setup {}
-
 -- go install github.com/bufbuild/buf-language-server/cmd/bufls@latest
 lspconfig.bufls.setup {}
-
 -- npm install -g dockerfile-language-server-nodejs
 lspconfig.dockerls.setup {}
 -- npm install @microsoft/compose-language-service
--- lua/plug.lua
+++ lua/plug.lua
@@ -136,6 +136,17 @@ require("lazy").setup({
         cmd="Copilot enable"
     },
 
+	{
+		"Exafunction/codeium.vim",
+		event = "BufEnter",
+		-- config = function()
+			-- vim.keymap.set('i', '<C-g>', function () return vim.fn['codeium#Accept']() end, { expr = true, silent = true })
+			-- vim.keymap.set('i', '<c-;>', function() return vim.fn['codeium#CycleCompletions'](1) end, { expr = true, silent = true })
+			-- vim.keymap.set('i', '<c-,>', function() return vim.fn['codeium#CycleCompletions'](-1) end, { expr = true, silent = true })
+			-- vim.keymap.set('i', '<c-x>', function() return vim.fn['codeium#Clear']() end, { expr = true, silent = true })
+		-- end
+	},
+
     {
         "nvimdev/lspsaga.nvim",
         config = function()
